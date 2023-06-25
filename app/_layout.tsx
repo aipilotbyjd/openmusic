7025b143291ec403a3a219e3ca465e2ee6b6c5a9
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-06-25 15:06:48 +0800

    dq

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,35 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { SplashScreen, Stack } from 'expo-router';
-import { useEffect } from 'react';
-import { useColorScheme } from 'react-native';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import { useColorScheme } from "react-native";
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: "(tabs)",
 };
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    SFProDisplayRegular: require("../assets/fonts/SF-Pro-Display-Regular"),
+    SFProDisplayBold: require("../assets/fonts/SF-Pro-Display-Bold"),
+    SFProDisplayMedium: require("../assets/fonts/SF-Pro-Display-Medium"),
+    SFProDisplaySemibold: require("../assets/fonts/SF-Pro-Display-Semibold"),
+    SFProTextRegular: require("../assets/fonts/SF-Pro-Text-Regular"),
+    SFProTextBold: require("../assets/fonts/SF-Pro-Text-Bold"),
+    SFProTextMedium: require("../assets/fonts/SF-Pro-Text-Medium"),
+    SFProTextSemibold: require("../assets/fonts/SF-Pro-Text-Semibold"),
     ...FontAwesome.font,
   });
 
@@ -40,10 +52,10 @@ function RootLayoutNav() {
 
   return (
     <>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+          <Stack.Screen name="modal" options={{ presentation: "modal" }} />
         </Stack>
       </ThemeProvider>
     </>
