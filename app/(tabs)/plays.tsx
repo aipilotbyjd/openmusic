04285208439fa95937167a86c48fe9f705efd264
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-06-27 00:39:03 +0800

    d

--- app/(tabs)/plays.tsx
+++ app/(tabs)/plays.tsx
@@ -154,6 +154,7 @@ const Plays: React.FC = () => {
               color="white"
               style={styles.arrowIcon}
             />
+            <Ionicons name="ios-search" size={24} color="white" />
           </View>
         </TouchableOpacity>
         {showContent && (
