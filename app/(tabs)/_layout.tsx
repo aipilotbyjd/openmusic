e06effc778f4cfff4cbc767fea2fbf5f13de0b21
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-06-26 13:19:12 +0800

    sqs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -25,6 +25,7 @@ export default function TabLayout() {
         tabBarActiveTintColor: Colors.dark.tint,
         tabBarStyle: {
           backgroundColor: Colors.dark.background,
+          paddingTop: 7,
         },
       }}
     >
